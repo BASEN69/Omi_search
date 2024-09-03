ae85d3ad60001c47c89a8d1ccddce65c736c5a53
TakuroSemba <171462421+BASEN69@users.noreply.github.com>
2024-09-03 07:06:32 +0000

    マップ修正ならず

--- app/javascript/images.js
+++ app/javascript/images.js
@@ -2,12 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
   const maxFileCount = 3; // 最大ファイル選択数
   const fileInput = document.querySelector('#images');
 
-  fileInput.addEventListener('change', function () {
-    if (fileInput.files.length > maxFileCount) {
-      alert(`最大で ${maxFileCount} 枚までしか選択できません。`);
-      // ファイルをリセットする
-      fileInput.value = '';
-    }
-  });
+  if (fileInput) {  // 要素が存在するか確認
+    fileInput.addEventListener('change', function () {
+      if (fileInput.files.length > maxFileCount) {
+        alert(`最大で ${maxFileCount} 枚までしか選択できません。`);
+        // ファイルをリセットする
+        fileInput.value = '';
+      }
+    });
+  } else {
+    console.error('Element with ID #images not found');
+  }
 });
 
