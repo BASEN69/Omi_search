37c12f02ae5abc13c3997c390aeb605b922ac539
TakuroSemba <171462421+BASEN69@users.noreply.github.com>
2024-10-04 09:19:59 +0000

    いいねの非同期化

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,8 +3,8 @@ class Public::FavoritesController < ApplicationController
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: post.id)
     favorite.save
-    flash[:notice] = "投稿にいいね！しました"
-    redirect_to request.referer
+
+    @post = post
     #リクエストを送信した元のページのURL
   end
 
@@ -12,7 +12,7 @@ class Public::FavoritesController < ApplicationController
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.find_by(post_id: post.id)
     favorite.destroy
-    flash[:notice] = "投稿へのいいね！を取り消しました"
-    redirect_to request.referer
+
+    @post = post
   end
 end
